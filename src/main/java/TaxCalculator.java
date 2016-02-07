712786de48b8348f16b1b5f7c49972dd83670260
Lynn Chin <lynchin@gmail.com>
2016-02-07 13:55:56 +1100

    Modify TaxCalculator class

--- src/main/java/TaxCalculator.java
+++ src/main/java/TaxCalculator.java
@@ -4,18 +4,31 @@ import java.util.ArrayList;
  * Created by lynnchin on 7/02/2016.
  */
 public class TaxCalculator {
-	private ArrayList<TaxItem> taxItems;
 
-	public TaxCalculator(ArrayList<TaxItem> taxItems) {
-		this.taxItems = taxItems;
+	private static ArrayList<TaxItem> initializeTaxItems() {
+		ArrayList<TaxItem> taxItems = new ArrayList<>();
+
+		TaxItem itemOne = new TaxItem(18200, 37000, 0.19, 0);
+		TaxItem itemTwo = new TaxItem(37000, 80000, 0.325, 3572);
+		TaxItem itemThree = new TaxItem(80000, 180000, 0.37, 17547);
+		TaxItem itemFour = new TaxItem(180000, Integer.MAX_VALUE, 0.45, 54547);
+
+		taxItems = new ArrayList<>();
+
+		taxItems.add(itemOne);
+		taxItems.add(itemTwo);
+		taxItems.add(itemThree);
+		taxItems.add(itemFour);
+
+		return taxItems;
 	}
 
-	public long calculate(long annualSalary)
-	throws Exception {
+	public static long calculate(long annualSalary)
+	throws PaySlipCalculatorException {
 		if (annualSalary < 0) {
-			throw new Exception("Annual Salary must be greater than zero");
+			throw new PaySlipCalculatorException("Annual Salary must be greater than zero");
 		}
-		for (TaxItem item : taxItems) {
+		for (TaxItem item : initializeTaxItems()) {
 			if (annualSalary > item.getMinLimit() && annualSalary <= item.getMaxLimit()) {
 				return Math.round((item.getAdditionCost() + (annualSalary - item.getMinLimit()) * item.getTaxRate()) / 12);
 			}
--- src/test/java/TaxCalculatorTest.java
+++ src/test/java/TaxCalculatorTest.java
@@ -1,43 +1,21 @@
 import junit.framework.TestCase;
-import org.junit.Before;
-
-import java.util.ArrayList;
 
 /**
  * Created by lynnchin on 7/02/2016.
  */
 public class TaxCalculatorTest extends TestCase {
-	private TaxCalculator calculator;
-	private ArrayList<TaxItem> taxItems;
 	
-	@Before
-	public void setUp(){
-		TaxItem itemOne = new TaxItem(18200,37000,0.19,0);
-		TaxItem itemTwo = new TaxItem(37000,80000,0.325,3572);
-		TaxItem itemThree = new TaxItem(80000,180000,0.37,17547);
-		TaxItem itemFour = new TaxItem(180000,Integer.MAX_VALUE,0.45,54547);
-		
-		taxItems = new ArrayList<>();
-
-		taxItems.add(itemOne);
-		taxItems.add(itemTwo);
-		taxItems.add(itemThree);
-		taxItems.add(itemFour);
-		
-		calculator = new TaxCalculator(taxItems);
-	}
-	
-	public void testWithInvalidAnnualIncome(){
+	public void testWithInvalidAnnualIncome() {
 		try {
-			calculator.calculate(-1000);
-		}catch(Exception ex){
-			assertEquals(ex.getMessage(),"Annual Salary must be greater than zero");
+			TaxCalculator.calculate(-1000);
+		} catch (Exception ex) {
+			assertEquals(ex.getMessage(), "Annual Salary must be greater than zero");
 		}
 	}
-	
-	public void testCalculateTaxableIncome() throws Exception{
-		double result = calculator.calculate(60050);
-		assertEquals(922,(long)result);
+
+	public void testCalculateTaxableIncome()
+	throws Exception {
+		double result = TaxCalculator.calculate(60050);
+		assertEquals(922, (long) result);
 	}
-	
 }
